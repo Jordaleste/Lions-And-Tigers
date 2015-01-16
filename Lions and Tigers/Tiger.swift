31bf0fd4abe0dabab2ce5770aac6282ef27767fb
Jordaleste <charlie@esnenterprises.com>
2015-01-16 16:28:18 -0500

    First Commit

---
+++ Lions and Tigers/Tiger.swift
@@ -0,0 +1,20 @@
+//
+//  Tiger.swift
+//  Lions and Tigers
+//
+//  Created by Charles Eison on 1/15/15.
+//  Copyright (c) 2015 Charles Eison. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+    
+    
+}
